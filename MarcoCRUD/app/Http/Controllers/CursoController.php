50e6b1cc11c7c95662566364017e39beae54efc3
Andrés Felipe Leal Forero <aflealf@unal.edu.co>
2023-04-27 17:56:35 -0500

    Develop Project Creation

---
+++ MarcoCRUD/app/Http/Controllers/CursoController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App;
+namespace App\Models\Curso;
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $cursos = Curso::all();
+        return response()->json($cursos);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $curso = Curso::create($request->all());
+        return response()->json($curso, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Curso $curso)
+    {
+        //
+        return $curso;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Curso $curso)
+    {
+        //
+        $curso->update($request->all());
+        return response()->json($curso, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Curso $curso)
+    {
+        //
+        $curso->delete();
+        return response()->json(null, 204);
+    }
+}
